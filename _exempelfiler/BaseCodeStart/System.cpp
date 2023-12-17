272c949f261e2460cce17970029f478ce0d77f89
Elias Bennaceur <eliasben00@gmail.com>
2023-12-17 11:05:04 +0100

    Base code added

---
+++ _exempelfiler/BaseCodeStart/System.cpp
@@ -0,0 +1,55 @@
+#include "Constants.h"
+#include "System.h"
+#include <SDL2/SDL.h>
+#include <iostream>
+
+namespace cwing 
+{
+
+	System::System()
+	{
+		std::cout << "*** System::System()\n";
+		
+		SDL_Init(SDL_INIT_EVERYTHING);
+		win = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED,
+			SDL_WINDOWPOS_CENTERED, 700, 600, 0);
+		ren = SDL_CreateRenderer(win, -1, 0);
+		TTF_Init();
+		font = TTF_OpenFont( (constants::gResPath + "fonts/arial.ttf").c_str(), 36);
+		std::cout << "resPath: " << constants::gResPath << std::endl;
+
+
+        Mix_OpenAudio(20050, AUDIO_S16SYS, 2, 4096);
+	    // Path to your own 'sounds' folder!
+  	    //musik = Mix_LoadWAV("/Users/kjellna/dev/cpp21/f13b/sounds/bgMusic.wav");
+	    music = Mix_LoadWAV( (constants::gResPath + "sounds/bgMusic.wav").c_str() );
+	    Mix_PlayChannel(-1, music, -1);
+	}
+
+
+	System::~System()
+	{
+		std::cout << "*** System::~System()\n";
+        Mix_FreeChunk(music);
+	    Mix_CloseAudio();
+		TTF_CloseFont(font);
+		TTF_Quit();
+		SDL_DestroyWindow(win);
+		SDL_DestroyRenderer(ren);
+		SDL_Quit();
+	}
+
+	SDL_Renderer* System::get_ren() const {
+		return ren;
+	}
+
+	TTF_Font* System::get_font() const {
+		return font;
+	}
+
+    Mix_Chunk* System::get_music() const{
+        return music;
+    }
+	
+	System sys; // Statiskt globalt objekt (definierad utanför funktioner.)
+}
---
+++ _exempelfiler/BaseCodeStart/System.h
@@ -0,0 +1,31 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include <string>
+
+namespace cwing 
+{
+
+	class System
+	{
+	public:
+		System();
+		~System();
+		SDL_Renderer* get_ren() const;
+		TTF_Font* get_font() const;
+		Mix_Chunk* get_music() const;
+
+	private:
+		SDL_Window* win;
+		SDL_Renderer* ren;
+		TTF_Font* font;
+		Mix_Chunk* music;
+	};
+	
+	extern System sys;
+}
+
+#endif
